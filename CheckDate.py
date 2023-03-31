71cc8e9f92d3d63c0e32a6cf3bb768b4c819dddd
crystalvega <110161341+crystalvega@users.noreply.github.com>
2023-03-31 15:59:06 +0500

    Update CheckDate.py

--- CheckDate.py
+++ CheckDate.py
@@ -6,9 +6,12 @@ date = str(dateraw.year) + '-' + str(dateraw.month) + '-' + str(dateraw.day)
 
 def ParseDate(folder):
     returndate = []
+    dateold = ''
     with open(folder, "r") as file:
         for line in file:
             dateold = line
+        if dateold == '':
+            return None
         for date in dateold.split('-'):
             returndate.append(int(date))
     return returndate
@@ -19,12 +22,15 @@ def DiffDays(dateold, datenow):
     return diff.days
     
 def CreateDate(folder):
-    with open(folder, "w") as file:
-        file.write(date)
+    if Path(folder).is_file():
+        with open(folder, "w") as file:
+            file.write(date)
 
 def CheckDate(folder, daysforcheck):
     if Path(folder).is_file():
         dateold = ParseDate(folder)
+        if dateold == None:
+            return True
         diff = DiffDays(dateold, dateraw)
         if diff >= daysforcheck:
             returnvalue = True
